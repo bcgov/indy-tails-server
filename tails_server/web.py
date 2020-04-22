369e474dc671142171e28ef1ad4d4f06b016fa81
Nicholas Rempel <nick@lucent.is>
2020-04-22 16:14:41 -0700

    Add some basic file handling to web
    Signed-off-by: Nicholas Rempel <nick@lucent.is>

--- tails_server/web.py
+++ tails_server/web.py
@@ -1,10 +1,37 @@
+import hashlib
+import base58
 from aiohttp import web
+from indy_vdr import ledger
 
 from .config.defaults import DEFAULT_WEB_HOST, DEFAULT_WEB_PORT
 
 
+
+REVOCATION_REGISTRY_ID_HEADER = "X-Revocation-Registry-ID"
+
+
 async def index(request):
-    return web.json_response({})
+
+    # Get revocation registry id
+    revocation_reg_id = request.headers.get(REVOCATION_REGISTRY_ID_HEADER)
+    if not revocation_reg_id:
+        web.Response(f"Missing header: {REVOCATION_REGISTRY_ID_HEADER}", status=400)
+
+    # Hash File
+    sha256 = hashlib.sha256()
+    while True:
+        chunk = await request.content.readany()
+        if not chunk:
+            break
+
+        sha256.update(chunk)
+
+    digest = sha256.hexdigest()
+    b58_digest = base58.b58encode(digest)
+
+    # Fetch revocation registry from ledger
+
+    return web.Response()
 
 
 def start(settings):
