1c4b50f87bbbd68f25a8c7b834115dd096ebcd20
Nicholas Rempel <nick@lucent.is>
2020-04-28 12:15:33 -0700

    create directory on startup
    Signed-off-by: Nicholas Rempel <nick@lucent.is>

--- tails_server/__init__.py
+++ tails_server/__init__.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 from .args import get_settings
 from .web import start
 
 
 def main():
     settings = get_settings()
+    Path(settings["storage_path"]).mkdir(parents=True, exist_ok=True)
     start(settings)
 
 
