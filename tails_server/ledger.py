7d17b9a8d6d5017126cb4b057d696a937c091332
Nicholas Rempel <nick@lucent.is>
2020-05-08 14:39:58 -0700

    always close pool
    Signed-off-by: Nicholas Rempel <nick@lucent.is>

--- tails_server/ledger.py
+++ tails_server/ledger.py
@@ -17,33 +17,38 @@ class BadRevocationRegistryIdError(Exception):
 
 
 async def get_rev_reg_def(genesis_txn_bytes, rev_reg_id, storage_path):
-    # Write the genesis transactions to the file system
-    with NamedTemporaryFile("w+b") as tmp_file:
-        # Let's test the genesis file before we store it permanently
-        tmp_file.write(genesis_txn_bytes)
-        tmp_file.seek(0)
-        # Try to connect to ledger
+    pool = None
+    try:
+        # Write the genesis transactions to the file system
+        with NamedTemporaryFile("w+b") as tmp_file:
+            # Let's test the genesis file before we store it permanently
+            tmp_file.write(genesis_txn_bytes)
+            tmp_file.seek(0)
+            # Try to connect to ledger
+            try:
+                pool = await indy_vdr.open_pool(transactions_path=tmp_file.name)
+            except indy_vdr.error.VdrError as e:
+                if e.code == indy_vdr.VdrErrorCode.INPUT:
+                    raise BadGenesisError()
+                else:
+                    raise
+
+        # Get transaction from ledger
         try:
-            pool = await indy_vdr.open_pool(transactions_path=tmp_file.name)
+            req = indy_vdr.ledger.build_get_revoc_reg_def_request(
+                None, rev_reg_id
+            )
         except indy_vdr.error.VdrError as e:
+            logger.info(e.code)
             if e.code == indy_vdr.VdrErrorCode.INPUT:
-                raise BadGenesisError()
+                raise BadRevocationRegistryIdError()
             else:
                 raise
 
-    # Get transaction from ledger
-    try:
-        req = indy_vdr.ledger.build_get_revoc_reg_def_request(
-            None, rev_reg_id
-        )
-    except indy_vdr.error.VdrError as e:
-        logger.info(e.code)
-        if e.code == indy_vdr.VdrErrorCode.INPUT:
-            raise BadRevocationRegistryIdError()
-        else:
-            raise
-
-    resp = await pool.submit_request(req)
+        resp = await pool.submit_request(req)
+    finally:
+        if pool:
+            pool.close()
 
     try:
         return resp["data"]
