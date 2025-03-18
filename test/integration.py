5894ef04baa20a4bbaa293af58eac3e407dd90a5
Gavinok <34443260+Gavinok@users.noreply.github.com>
2025-03-18 10:13:00 -0700

    Remove dependency on indy-sdk for testing (#109)
    Remove dependency on indy-sdk for testing 
    
    Signed-off-by: Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>

--- test/integration.py
+++ test/integration.py
@@ -6,41 +6,70 @@ import json
 import os
 from random import randrange
 from tempfile import NamedTemporaryFile
+from typing import Any
 
 import aiofiles
 import aiohttp
 import base58
-import indy
 import indy_vdr
 import nacl.signing
+from anoncreds import CredentialDefinition, RevocationRegistryDefinition, Schema
+from indy_vdr.request import Request
 from rich import print as rprint
 from rich.panel import Panel
 
-ISSUER = {
+ISSUER: dict[str, str] = {
     "seed": "00000000000000000000000000000000",
     "did": "4QxzWk3ajdnEA37NdNU5Kt",
-    "wallet_config": json.dumps({"id": "issuer_wallet"}),
-    "wallet_credentials": json.dumps({"key": "issuer_wallet_key"}),
 }
 
-SCHEMA = {
+SCHEMA: dict[str, str | list[str]] = {
     "name": "DL",
     "version": f"{randrange(10000)}.{randrange(10000)}.{randrange(10000)}",
-    "attributes": '["age", "sex", "height", "name"]',
+    "attributes": ["age", "sex", "height", "name"],
 }
 
-CRED_DEF = {
+CRED_DEF: dict[str, str | bool] = {
     "tag": "cred_def_tag",
     "type": "CL",
-    "config": json.dumps({"support_revocation": True}),
+    "support_revocation": True,
 }
 
-REVOC_REG_DEF = {
-    "config": json.dumps({"max_cred_num": 5, "issuance_type": "ISSUANCE_ON_DEMAND"}),
+REVOC_REG_DEF: dict[str, str | int] = {
+    "registry_type": "CL_ACCUM",
+    "max_cred_num": 5,
+    "issuance_type": "ISSUANCE_ON_DEMAND",
+    "tag": "rev_reg_tag",
 }
 
 
-def log_event(msg, panel=False, error=False):
+async def create_did(seed: str) -> tuple[str, str]:
+    # TODO: remove need for nacl and only use askar
+    # Generate an Ed25519 keypair using the same method as Indy SDK
+    signing_key = nacl.signing.SigningKey(seed.encode())
+    verify_key = signing_key.verify_key
+    # Extract the first 16 bytes of the public key
+    did_bytes = verify_key.encode()[:16]
+
+    # Convert to base58 to get the DID
+    did = base58.b58encode(did_bytes).decode()
+    verify_key = base58.b58encode(verify_key.encode()).decode()
+    return did, verify_key
+
+
+def create_schema(
+    issuer_did: str, name: str, version: str, attrs: list[str]
+) -> tuple[Schema, str]:
+    schema_id = f"{issuer_did}:2:{name}:{version}"
+    return (
+        Schema.create(
+            issuer_id=issuer_did, name=name, version=version, attr_names=attrs
+        ),
+        schema_id,
+    )
+
+
+def log_event(msg: str, panel: bool = False, error: bool = False):
     if not error:
         msg = f"[bright_green]{msg}"
     else:
@@ -52,118 +81,122 @@ def log_event(msg, panel=False, error=False):
     rprint(msg)
 
 
-def sign_request(req, seed):
+def sign_request(req: Request, seed: str):
     key = nacl.signing.SigningKey(seed.encode("ascii"))
     signed = key.sign(req.signature_input)
     req.set_signature(signed.signature)
     return req
 
 
-async def connect_to_ledger(genesis_txn_path):
+async def connect_to_ledger(genesis_txn_path: str):
     return await indy_vdr.open_pool(transactions_path=genesis_txn_path)
 
 
 # Register Issuer DID as Endorser using Steward
-async def register_issuer_did(pool):
-    issuer_wallet_handle = ISSUER["wallet"]
+async def register_issuer_did(pool: indy_vdr.Pool):
 
     log_event("Generating and storing Steward DID and verkey...")
     steward_seed = "000000000000000000000000Steward1"
-    did_json = json.dumps({"seed": steward_seed})
-    steward_did, steward_verkey = await indy.did.create_and_store_my_did(
-        issuer_wallet_handle, did_json
-    )
 
-    log_event("Generating and storing Issuer DID and verkey...")
-    did_json = json.dumps({"seed": ISSUER["seed"]})
-    issuer_did, issuer_verkey = await indy.did.create_and_store_my_did(
-        issuer_wallet_handle, did_json
-    )
+    steward_did, steward_verkey = await create_did(steward_seed)
 
-    log_event("Registering issuer DID...")
+    log_event(
+        f"Generating and storing Issuer DID and verkey {steward_did}:{steward_verkey}..."
+    )
+    issuer_did, issuer_verkey = await create_did(ISSUER["seed"])
+
+    log_event(f"Registering issuer DID {issuer_did}:{issuer_verkey}...")
     req = indy_vdr.ledger.build_nym_request(
         steward_did, issuer_did, verkey=issuer_verkey, role="ENDORSER"
     )
-    resp = await pool.submit_request(sign_request(req, steward_seed))
+    await pool.submit_request(sign_request(req, steward_seed))
 
 
-async def create_issuer_wallet():
-    await indy.wallet.create_wallet(
-        ISSUER["wallet_config"], ISSUER["wallet_credentials"]
-    )
-    ISSUER["wallet"] = await indy.wallet.open_wallet(
-        ISSUER["wallet_config"], ISSUER["wallet_credentials"]
+async def publish_schema(pool: indy_vdr.Pool):
+    schema, schema_id = create_schema(
+        ISSUER["did"], SCHEMA["name"], SCHEMA["version"], attrs=SCHEMA["attributes"]
     )
 
+    # Add missing fields
+    tmp = schema.to_json()
+    json1_data: dict[str, Any] = json.loads(tmp)
+    json1_data["ver"] = "1.0"
+    json1_data["id"] = schema_id
 
-async def publish_schema(pool):
-    ISSUER["schema_id"], ISSUER["schema"] = await indy.anoncreds.issuer_create_schema(
-        ISSUER["did"], SCHEMA["name"], SCHEMA["version"], SCHEMA["attributes"]
-    )
+    ISSUER["schema_id"], ISSUER["schema"] = schema_id, json.dumps(json1_data)
+
     req = indy_vdr.ledger.build_schema_request(ISSUER["did"], ISSUER["schema"])
     resp = await pool.submit_request(sign_request(req, ISSUER["seed"]))
+    CRED_DEF["seqNo"] = resp["txnMetadata"]["seqNo"]
     schema_dict = json.loads(ISSUER["schema"])
     schema_dict["seqNo"] = resp["txnMetadata"]["seqNo"]
     ISSUER["schema"] = json.dumps(schema_dict)
+    return json.dumps(schema_dict)
 
 
-async def publish_cred_def(pool):
-    (
-        ISSUER["cred_def_id"],
-        ISSUER["cred_def"],
-    ) = await indy.anoncreds.issuer_create_and_store_credential_def(
-        ISSUER["wallet"],
-        ISSUER["did"],
-        ISSUER["schema"],
-        CRED_DEF["tag"],
-        CRED_DEF["type"],
-        CRED_DEF["config"],
+def make_cred_def_id() -> str:
+    """Derive the ID for a credential definition."""
+    return f"{ISSUER['did']}:3:{CRED_DEF['type']}:{CRED_DEF['seqNo']}:{CRED_DEF['tag']}"
+
+
+async def publish_cred_def(pool: indy_vdr.Pool):
+
+    (cred_def, _, _) = CredentialDefinition.create(
+        schema_id=ISSUER["schema_id"],
+        schema=ISSUER["schema"],
+        issuer_id=ISSUER["did"],
+        tag=CRED_DEF["tag"],
+        signature_type=CRED_DEF["type"],
+        support_revocation=CRED_DEF["support_revocation"],
     )
+
+    # Add missing fields needed by indy_vdr
+    cd = cred_def.to_dict()
+    cd["ver"] = "1.0"
+    cd["id"] = make_cred_def_id()
+    cd["schemaId"] = str(CRED_DEF["seqNo"])
+
+    ISSUER["cred_def"] = cd
+
     req = indy_vdr.ledger.build_cred_def_request(ISSUER["did"], ISSUER["cred_def"])
-    ISSUER["cred_def"] = json.dumps(
-        await pool.submit_request(sign_request(req, ISSUER["seed"]))
-    )
+    json.dumps(await pool.submit_request(sign_request(req, ISSUER["seed"])))
 
 
-async def publish_revoc_reg(pool, tag):
-    ISSUER["tails_writer_config"] = json.dumps({"base_dir": "tails", "uri_pattern": ""})
-    ISSUER["tails_writer"] = await indy.blob_storage.open_writer(
-        "default", ISSUER["tails_writer_config"]
+async def publish_revoc_reg(pool: indy_vdr.Pool, tag):
+    rev_reg_def, rev_reg_def_private = RevocationRegistryDefinition.create(
+        cred_def_id=make_cred_def_id(),
+        cred_def=ISSUER["cred_def"],
+        issuer_id=ISSUER["did"],
+        tag=tag,
+        registry_type=REVOC_REG_DEF["registry_type"],
+        max_cred_num=REVOC_REG_DEF["max_cred_num"],
     )
-    (
-        ISSUER["rev_reg_id"],
-        ISSUER["rev_reg_def"],
-        ISSUER["rev_reg_entry"],
-    ) = await indy.anoncreds.issuer_create_and_store_revoc_reg(
-        ISSUER["wallet"],
-        ISSUER["did"],
-        None,
-        tag,
-        ISSUER["cred_def_id"],
-        REVOC_REG_DEF["config"],
-        ISSUER["tails_writer"],
-    )
-    req = indy_vdr.ledger.build_revoc_reg_def_request(
-        ISSUER["did"], ISSUER["rev_reg_def"]
-    )
+
+    # add missing fields
+    json_data = rev_reg_def.to_dict()
+    json_data["id"] = f"{ISSUER['did']}:4:{make_cred_def_id()}:CL_ACCUM:{tag}"
+    json_data["ver"] = "1.0"
+    json_data["schemaId"] = CRED_DEF["seqNo"]
+    json_data["value"]["issuanceType"] = REVOC_REG_DEF["issuance_type"]
+
+    rev_reg_def = json_data
+
+    req = indy_vdr.ledger.build_revoc_reg_def_request(ISSUER["did"], rev_reg_def)
+
     return (await pool.submit_request(sign_request(req, ISSUER["seed"])))["txn"]["data"]
 
 
 async def run_tests(genesis_url, tails_server_url):
-    # rich_traceback_install()
-    session = aiohttp.ClientSession()
-
-    log_event("Setting up indy environment...")
-    log_event("Downloading genesis transactions...")
-    async with session.get(genesis_url) as resp:
-        with NamedTemporaryFile("w+b", delete=False) as genesis_file:
-            genesis_file.write(await resp.read())
-            genesis_file.seek(0)
+    async with aiohttp.ClientSession() as session:
+        log_event("Setting up indy environment...")
+        log_event("Downloading genesis transactions...")
+        async with session.get(genesis_url) as resp:
+            with NamedTemporaryFile("w+b", delete=False) as genesis_file:
+                genesis_file.write(await resp.read())
+                genesis_file.seek(0)
 
     log_event("Connecting to ledger...")
     pool = await connect_to_ledger(genesis_file.name)
-    log_event("Creating wallet...")
-    await create_issuer_wallet()
     log_event("Registering DID to ledger...")
     await register_issuer_did(pool)
     log_event("Publishing schema to ledger...")
@@ -232,85 +265,79 @@ async def run_tests(genesis_url, tails_server_url):
 
 async def test_happy_path(genesis_path, tails_server_url, revo_reg_def):
     log_event("Testing happy path...", panel=True)
-    session = aiohttp.ClientSession()
+    async with aiohttp.ClientSession() as session:
+        with (
+            open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
+            open(genesis_path, "rb") as genesis_file,
+        ):
+            async with session.put(
+                f"{tails_server_url}/{revo_reg_def['id']}",
+                data={"genesis": genesis_file, "tails": tails_file},
+            ) as resp:
+                assert resp.status == 200
+                log_event("Passed")
 
-    with (
-        open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
-        open(genesis_path, "rb") as genesis_file,
-    ):
-        async with session.put(
-            f"{tails_server_url}/{revo_reg_def['id']}",
-            data={"genesis": genesis_file, "tails": tails_file},
-        ) as resp:
-            assert resp.status == 200
-    log_event("Passed")
-
-    # Find matching tails file
-    async with session.get(
-        f"{tails_server_url}/match/{revo_reg_def['credDefId']}"
-    ) as resp:
-        # Upload is complete so this should succeed
-        assert resp.status == 200
-        matches = json.loads(await resp.read())
-        assert matches
+            # Find matching tails file
+            async with session.get(
+                f"{tails_server_url}/match/{revo_reg_def['credDefId']}"
+            ) as resp:
+                # Upload is complete so this should succeed
+                assert resp.status == 200
+                matches = json.loads(await resp.read())
+                assert matches
 
 
 async def test_bad_revoc_reg_id_404(genesis_path, tails_server_url, revo_reg_def):
     log_event("Testing bad revocation registry id...", panel=True)
-    session = aiohttp.ClientSession()
-
-    with (
-        open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
-        open(genesis_path, "rb") as genesis_file,
-    ):
-        async with session.put(
-            f"{tails_server_url}/bad-id",
-            data={"genesis": genesis_file, "tails": tails_file},
-        ) as resp:
-            assert resp.status == 400
-
+    async with aiohttp.ClientSession() as session:
+        with (
+            open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
+            open(genesis_path, "rb") as genesis_file,
+        ):
+            async with session.put(
+                f"{tails_server_url}/bad-id",
+                data={"genesis": genesis_file, "tails": tails_file},
+            ) as resp:
+                assert resp.status == 400
     log_event("Passed")
 
 
 async def test_upload_already_exist(genesis_path, tails_server_url, revo_reg_def):
     log_event("Testing upload already exists...", panel=True)
-    session = aiohttp.ClientSession()
-
-    with (
-        open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
-        open(genesis_path, "rb") as genesis_file,
-    ):
-        # First upload
-        async with session.put(
-            f"{tails_server_url}/{revo_reg_def['id']}",
-            data={"genesis": genesis_file, "tails": tails_file},
-        ) as resp:
-            assert resp.status == 200
-
-    with (
-        open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
-        open(genesis_path, "rb") as genesis_file,
-    ):
-        # Second upload
-        async with session.put(
-            f"{tails_server_url}/{revo_reg_def['id']}",
-            data={"genesis": genesis_file, "tails": tails_file},
-        ) as resp:
-            assert resp.status == 409
+    async with aiohttp.ClientSession() as session:
+        with (
+            open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
+            open(genesis_path, "rb") as genesis_file,
+        ):
+            # First upload
+            async with session.put(
+                f"{tails_server_url}/{revo_reg_def['id']}",
+                data={"genesis": genesis_file, "tails": tails_file},
+            ) as resp:
+                assert resp.status == 200
+        with (
+            open(revo_reg_def["value"]["tailsLocation"], "rb") as tails_file,
+            open(genesis_path, "rb") as genesis_file,
+        ):
+            # Second upload
+            async with session.put(
+                f"{tails_server_url}/{revo_reg_def['id']}",
+                data={"genesis": genesis_file, "tails": tails_file},
+            ) as resp:
+                assert resp.status == 409
 
     log_event("Passed")
 
 
 async def test_upload_bad_tails_file(genesis_path, tails_server_url, revo_reg_def):
     log_event("Testing bad tails file...", panel=True)
-    session = aiohttp.ClientSession()
-
-    with open(genesis_path, "rb") as genesis_file:
-        async with session.put(
-            f"{tails_server_url}/{revo_reg_def['id']}",
-            data={"genesis": genesis_file, "tails": b"bad bytes"},
-        ) as resp:
-            assert resp.status == 400
+    async with aiohttp.ClientSession() as session:
+        with open(genesis_path, "rb") as genesis_file:
+            async with session.put(
+                f"{tails_server_url}/{revo_reg_def['id']}",
+                data={"genesis": genesis_file, "tails": b"bad bytes"},
+            ) as resp:
+                assert resp.status == 400
 
     log_event("Passed")
 
@@ -330,6 +357,7 @@ async def test_bad_content_type(genesis_path, tails_server_url, revo_reg_def):
         ) as resp:
             assert resp.status == 400
 
+    await session.close()
     log_event("Passed")
 
 
@@ -346,6 +374,7 @@ async def test_bad_field_order(genesis_path, tails_server_url, revo_reg_def):
             data={"tails": tails_file, "genesis": genesis_file},
         ) as resp:
             assert resp.status == 400
+    await session.close()
     log_event("Passed")
 
 
@@ -380,9 +409,10 @@ async def test_race_upload(genesis_path, tails_server_url, revo_reg_def):
     # and eventually returns 409 when it can read the file
     # since the file already exists
     resp1, resp2 = await asyncio.gather(upload(True), upload(False))
-    assert resp1.status == 200
-    assert resp2.status == 409
+    assert resp1.status == 200, f"resp1.status {resp1.status}"
+    assert resp2.status == 409, f"resp2.status {resp2.status}"
 
+    await session.close()
     log_event("Passed")
 
 
@@ -429,6 +459,7 @@ async def test_race_download(genesis_path, tails_server_url, revo_reg_def):
     resp1, resp2 = await asyncio.gather(upload(), download())
     assert resp1.status == 200
 
+    await session.close()
     log_event("Passed")
 
 
@@ -444,12 +475,13 @@ async def test_put_file_by_hash(tails_server_url):
     with aiohttp.MultipartWriter("mixed") as mpwriter:
         file.seek(0)
         mpwriter.append(file.read())
-        session = aiohttp.ClientSession()
-        async with session.put(
-            f"{tails_server_url}/hash/{tails_hash}",
-            data=mpwriter,
-        ) as resp:
-            assert resp.status == 200
+        async with aiohttp.ClientSession() as session:
+            async with session.put(
+                f"{tails_server_url}/hash/{tails_hash}",
+                data=mpwriter,
+            ) as resp:
+                assert resp.status == 200
+            await session.close()
 
     file.close()
     os.remove("test_tails.bin")
@@ -467,13 +499,13 @@ async def test_put_file_by_hash_x_version_tag(tails_server_url):
     with aiohttp.MultipartWriter("mixed") as mpwriter:
         file.seek(0)
         mpwriter.append(file.read())
-        session = aiohttp.ClientSession()
-        async with session.put(
-            f"{tails_server_url}/hash/{tails_hash}",
-            data=mpwriter,
-        ) as resp:
-            assert resp.status == 400
-            assert await resp.text() == 'Tails file must start with "00 02".'
+        async with aiohttp.ClientSession() as session:
+            async with session.put(
+                f"{tails_server_url}/hash/{tails_hash}",
+                data=mpwriter,
+            ) as resp:
+                assert resp.status == 400
+                assert await resp.text() == 'Tails file must start with "00 02".'
 
     file.close()
     os.remove("test_tails_x_version_tag.bin")
@@ -491,13 +523,13 @@ async def test_put_file_by_hash_x_file_size(tails_server_url):
     with aiohttp.MultipartWriter("mixed") as mpwriter:
         file.seek(0)
         mpwriter.append(file.read())
-        session = aiohttp.ClientSession()
-        async with session.put(
-            f"{tails_server_url}/hash/{tails_hash}",
-            data=mpwriter,
-        ) as resp:
-            assert resp.status == 400
-            assert await resp.text() == "Tails file is not the correct size."
+        async with aiohttp.ClientSession() as session:
+            async with session.put(
+                f"{tails_server_url}/hash/{tails_hash}",
+                data=mpwriter,
+            ) as resp:
+                assert resp.status == 400
+                assert await resp.text() == "Tails file is not the correct size."
 
     file.close()
     os.remove("test_tails_x_file_size.bin")
