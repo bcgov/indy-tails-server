889c1b41d239bb1657855ad1ca752604e40156d9
Caleb Siebach <caleb@indicio.tech>
2025-07-14 13:21:19 -0600

    chore: adding in json logging
    Signed-off-by: Caleb Siebach <caleb@indicio.tech>

--- tails_server/logging.py
+++ tails_server/logging.py
@@ -1,29 +1,13 @@
-import logging
-from logging.config import fileConfig
-from os import path
+from .loadlogger import LoggingConfigurator
 
-DEFAULT_LOGGING_CONFIG_PATH = path.join(
-    path.dirname(path.abspath(__file__)), "config", "default_logging_config.ini"
-)
-
-
-class BadLogConfigError(Exception):
-    pass
-
-
-def configure(logging_config_path: str = None, log_level: str = None):
-    if logging_config_path is not None:
-        config_path = logging_config_path
-    else:
-        config_path = DEFAULT_LOGGING_CONFIG_PATH
-
-    try:
-        fileConfig(config_path, disable_existing_loggers=False)
-    except KeyError:
-        # Oddly, fileConfig raises "KeyError: 'formatters'" if file not found
-        logging.root.error(f"Logging config file not found: {config_path}")
-        raise BadLogConfigError()
-
-    if log_level:
-        log_level = log_level.upper()
-        logging.root.setLevel(log_level)
+def configure_logging(settings):
+    """Perform common app configuration."""
+    # Set up logging
+    log_config = settings['log_config']
+    log_level = settings['log_level']
+    log_file = settings['log_file']
+    LoggingConfigurator.configure(
+        log_config_path=log_config,
+        log_level=log_level,
+        log_file=log_file,
+    )
